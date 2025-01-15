014cdc586b97d178ea0db56d04900502d39a2e71
LucaAgostinho <lucaabenda@icloud.com>
2025-01-15 20:29:12 -0300

    FEAT: Add a dict with locators
    The LOCATORS dict will have all of our locators and their respective format. This dict will help us identify what webelement are we interacting with on higher levels of the code  because of the names of its keys.

---
+++ config/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+LOCATORS = {
+    'select_portuguese' : (By.XPATH , '//div[@id="langSelect-PT-BR"]'), 
+    'big_cookie' : (By.XPATH, '//div[@id="bigCookie"]')
+}
