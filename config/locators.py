3f7f4dc80221d966e18d83fa62990ff763f2dbbe
LucaAgostinho <lucaabenda@icloud.com>
2025-01-15 20:46:06 -0300

    FIX: Fixing BigCookie locator

--- config/locators.py
+++ config/locators.py
@@ -2,5 +2,5 @@ from selenium.webdriver.common.by import By
 
 LOCATORS = {
     'select_portuguese' : (By.XPATH , '//div[@id="langSelect-PT-BR"]'), 
-    'big_cookie' : (By.XPATH, '//div[@id="bigCookie"]')
+    'big_cookie' : (By.XPATH, '//button[@id="bigCookie"]')
 }
