5dab874fdfdea834be48a9eed519a5af0b773781
LucaAgostinho <lucaabenda@icloud.com>
2025-01-15 22:33:09 -0300

    FEAT: Add main file
    For now this file is only here for testing and will be modified throughout the project. When everything is ready, this file will load the main execution logic of the project, instance of the created classes and the call of their functions

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+    For now this file is only here for testing and will be modified throughout the project.
+    When everything is ready, this file will load the main execution logic of the project,
+    instance of the created classes and the call of their functions
+"""
+
+from utils.CookieClickerDriver import CookieClickerDriver
+
+cookieDriver = CookieClickerDriver()
+cookieDriver.open_url()
+cookieDriver.select_language()
+
+while True:
+    cookieDriver.click_big_cookie()
