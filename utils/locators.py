549295a0c7840f206249bf240bc19f5d740b4aef
LucaAgostinho <lucaabenda@icloud.com>
2025-01-16 22:36:03 -0300

    FEAT: Add cookie_balance locator

--- utils/locators.py
+++ utils/locators.py
@@ -3,5 +3,6 @@ from selenium.webdriver.common.by import By
 LOCATORS = {
     'select_portuguese' : (By.XPATH , '//div[@id="langSelect-PT-BR"]'), 
     'big_cookie' : (By.XPATH, '//button[@id="bigCookie"]'),
-    'buildings' : (By.XPATH, '//div[@class="product unlocked enabled"]')
+    'buildings' : (By.XPATH, '//div[@class="product unlocked enabled"]'),
+    'cookie_balance' : (By.XPATH, '//div[@id="cookies"]')
 }
