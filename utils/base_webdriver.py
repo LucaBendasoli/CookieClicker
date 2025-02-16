baf5121ee042806167490c8d5a3866eba0549232
LucaAgostinho <lucaabenda@icloud.com>
2025-02-16 18:28:36 -0300

    refactor: remove unnecessary import in base_webdriver file

--- utils/base_webdriver.py
+++ utils/base_webdriver.py
@@ -2,7 +2,6 @@
 IT SHOULD BE INHERITED BY A CLASS THAT IMPLEMENTS THE URL ATTRIBUTE."""
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
