53ca5741a637fcb22b18fc85b1faf51e6281d132
LucaAgostinho <lucaabenda@icloud.com>
2025-02-16 17:16:43 -0300

    docs: improve docstrings in cookie_clicker_webdriver file

--- utils/cookie_clicker_webdriver.py
+++ utils/cookie_clicker_webdriver.py
@@ -1,17 +1,20 @@
+"""Module that contains the webdriver class for Cookie Clicker game."""
+
 from typing import Iterable
 from .locators import LOCATORS
 from .base_webdriver import WebDriver
 
 class CookieClickerDriver(WebDriver):
+    """This class inherits the base webdriver class and implements methods that
+    are specific to the Cookie Clicker game like clicking the big cookie, buying
+    builds and upgrades."""
 
     def __init__(self):
         super().__init__()
         self.url = 'https://orteil.dashnet.org/cookieclicker/'
 
     def select_language(self) -> None:
-
         """Selects the language in the game"""
-
         self.click_webelement(LOCATORS['select_portuguese'])
 
     def accept_cookies(self) -> None:
@@ -23,15 +26,11 @@ class CookieClickerDriver(WebDriver):
             pass
 
     def click_big_cookie(self) -> None:
-
         """Clicks the big cookie in the game"""
-
         self.click_webelement(LOCATORS['big_cookie'])
 
     def look_for_buildings_to_buy(self) -> None:
-
         """Looks for buildings to buy in the game"""
-
         try:
             buildings = self.find_webelement(LOCATORS['buildings'], 0)
         except:
@@ -42,9 +41,7 @@ class CookieClickerDriver(WebDriver):
         buildings[0].click()
 
     def look_for_upgrades_to_buy(self) -> None:
-
         """Looks for upgrades to buy in the game"""
-
         try:
             upgrade = self.find_webelement(LOCATORS['upgrade'], 0)
         except:
