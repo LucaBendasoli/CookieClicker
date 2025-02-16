04876e342c4dd8452b3f99de24fd40216f45b78a
LucaAgostinho <lucaabenda@icloud.com>
2025-02-16 16:08:36 -0300

    refactor: improve readability and organization

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 """
-    Main file that orchestrates the whole process of playing the Cookie Clicker game.
+    Main file that orchestrates the whole process of playing Cookie Clicker.
 """
 
 from utils.CookieClickerDriver import CookieClickerDriver
 
-cookieDriver = CookieClickerDriver()
-cookieDriver.open_url()
-cookieDriver.select_language()
+def start_cookie_clicker() -> CookieClickerDriver:
+    """Starts the Cookie Clicker game, selects portuguese as
+    the language and returns the driver object."""
+    cookie_driver = CookieClickerDriver()
+    cookie_driver.open_url()
+    cookie_driver.select_language()
+    return cookie_driver
 
-while True:
-    cookieDriver.click_big_cookie()
-    cookieDriver.look_for_upgrades_to_buy()
-    cookieDriver.look_for_buildings_to_buy()
+def click_and_buy_forever(cookie_driver: CookieClickerDriver) -> None:
+    """Run a infinite loop that clicks the big cookie and looks
+    for upgrades and buildings to buy as soon as they are available."""
+    while True:
+        cookie_driver.click_big_cookie()
+        cookie_driver.look_for_upgrades_to_buy()
+        cookie_driver.look_for_buildings_to_buy()
+
+def main() -> None:
+    """Main function that starts the game and plays it forever."""
+    cookie_driver = start_cookie_clicker()
+    click_and_buy_forever(cookie_driver)
+
+if __name__ == '__main__':
+    main()
