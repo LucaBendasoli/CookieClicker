99b3119f583f1e3e23232bcae06676ccd6b1aaf2
LucaAgostinho <lucaabenda@icloud.com>
2025-02-16 17:25:31 -0300

    docs: add docstrings to locators file

--- utils/locators.py
+++ utils/locators.py
@@ -1,10 +1,13 @@
+"""Module used for storing all locators used in the automation."""
+
 from selenium.webdriver.common.by import By
 
+# Dictionary that contains locators for the Cookie Clicker game
 LOCATORS = {
-    'accept_cookies' : (By.XPATH, '//a[contains(text(), "Got it!")]'),
-    'select_portuguese' : (By.XPATH , '//div[@id="langSelect-PT-BR"]'),
-    'big_cookie' : (By.XPATH, '//button[@id="bigCookie"]'),
-    'buildings' : (By.XPATH, '//div[@class="product unlocked enabled"]'),
-    'cookie_balance' : (By.XPATH, '//div[@id="cookies"]'),
-    'upgrade' : (By.XPATH, '//div[@class="crate upgrade enabled"]')
+    'accept_cookies' : (By.XPATH, '//a[contains(text(), "Got it!")]'),    # Accept website cookies
+    'select_portuguese' : (By.XPATH , '//div[@id="langSelect-PT-BR"]'),   # Select portuguese
+    'big_cookie' : (By.XPATH, '//button[@id="bigCookie"]'),               # Big cookie button
+    'buildings' : (By.XPATH, '//div[@class="product unlocked enabled"]'), # Buildings to buy
+    'cookie_balance' : (By.XPATH, '//div[@id="cookies"]'),                # Cookie balance
+    'upgrade' : (By.XPATH, '//div[@class="crate upgrade enabled"]')       # Upgrades to buy
 }
